c51e811fdd7e15912feb7a87215477d124abca49
Johannes Sommer <jsommer@geocledian.com>
2020-04-25 18:20:43 +0200

    initial map

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,19 @@ import store from './store'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 
+import 'leaflet/dist/leaflet.css';
+
+// start fix for missing markers
+import { Icon } from 'leaflet';
+
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+// end fix for missing markers
+
 Vue.use(VueMaterial)
 
 Vue.config.productionTip = false
---
+++ frontend/src/views/Map.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <div class="map">
+    <p>MapView</p>
+    <raise-map />
+  </div>
+</template>
+
+<script>
+import raiseMap from '../components/Map.vue'
+
+export default {
+  name: 'MapView',
+  components: {
+    raiseMap
+  }
+}
+</script>
