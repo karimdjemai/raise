6c19a602fd747665c755ad122b0a7361bd76cfa1
Adrian T <adrian.tuiu7@gmail.com>
2020-04-26 15:07:53 +0000

    Added shortage rebalancing

--- raise/src/optimization_engine/model.py
+++ raise/src/optimization_engine/model.py
@@ -11,11 +11,13 @@ class Model():
     def run(self, inputs):
         model = ConcreteModel()
 
-        model.Locations = Set(initialize = inputs.Locations, ordered = True)
-        model.Resources = Set(initialize = inputs.Resources, ordered = True)
+        model.Locations = Set(initialize = inputs.locations, ordered = True)
+        model.Resources = Set(initialize = inputs.resources, ordered = True)
 
         demand = self.preprocess_demand(inputs)
         availability = self.preprocess_availability(inputs)
+        shortage = self.calculate_shortage(inputs.resources, inputs.demand, inputs.availabilities)
+        demand, availability = self.distribute_shortage(inputs.factor, demand, availabilities, shortage)
 
         model.demand = Param(model.Locations, model.Resources, initialize = demand, default = 0)
         model.availability = Param(model.Locations, model.Resources, initialize = availability, default = 0)
@@ -39,6 +41,49 @@ class Model():
         results = opt.solve(model, tee=True)
         results.write()
 
+    def calculate_shortage(self, resources, demands, availabilities):
+        shortage = dict()
+
+        for i in demands:
+            for r in resources:
+                if i[1] == r:
+                    if r not in shortage:
+                        shortage[r] = 0
+
+                    shortage[r] += demands[i] - availabilities[i]
+
+        return shortage
+    
+    def distribute_shortage(self, factor, demand, availabilities, shortage):
+        for r in shortage:
+            if shortage[r] > 0:
+                for i in demand:
+                    if i[1] == r:
+                        demand[i] = round(demand[i] - shortage[r] * factor[i[0]])
+            elif shortage[r] < 0:
+                for i in availabilities:
+                    if i[1] == r:
+                        demand[i] = round(demand[i] * shortage[r] * (1 - factor[i[0]]))
+            else:
+                continue
+
+        total_demand = dict()
+        total_availability = dict()
+
+        for i in demand:
+            if i[1] not in total_demand:
+                total_demand[i[1]] = 0
+            
+            total_demand[i[1]] += demand[i]
+
+        for i in availabilities:
+            if i[1] not in total_availability:
+                total_availability[i[1]] = 0
+            
+            total_availability[i[1]] += availabilities[i]
+                
+        return (demand, availabilities)
+                        
     def preprocess_demand(self, inputs):
         return inputs.demand
     
@@ -46,20 +91,20 @@ class Model():
         return inputs.availabilities
 
 if __name__ == '__main__':
-    Locations = set(['North', 'South', 'West', 'East'])
-    Resources = set(['Ventilators', 'Doctors', 'Nurses'])
+    locations = set(['North', 'South', 'West', 'East'])
+    resources = set(['Ventilators', 'Doctors', 'Nurses'])
 
     demand = dict()
-    demand[('North', 'Ventilators')] = 5
+    demand[('North', 'Ventilators')] = 20
     demand[('North', 'Doctors')] = 10
     demand[('North', 'Nurses')] = 50
-    demand[('South', 'Ventilators')] = 100
+    demand[('South', 'Ventilators')] = 70
     demand[('South', 'Doctors')] = 40
     demand[('South', 'Nurses')] = 100
-    demand[('West', 'Ventilators')] = 20
+    demand[('West', 'Ventilators')] = 30
     demand[('West', 'Doctors')] = 100
     demand[('West', 'Nurses')] = 200
-    demand[('East', 'Ventilators')] = 5
+    demand[('East', 'Ventilators')] = 40
     demand[('East', 'Doctors')] = 100
     demand[('East', 'Nurses')] = 300
 
@@ -91,19 +136,27 @@ if __name__ == '__main__':
     distance[('East', 'South')] = 100
     distance[('East', 'West')] = 300
 
+    factor = dict()
+    factor['North'] = 0.22
+    factor['South'] = 0.38
+    factor['East'] = 0.3
+    factor['West'] = 0.1
+
     class Inputs():
-        Locations = set()
-        Resources = set()
+        locations = set()
+        resources = set()
         demand = dict()
         availabilities = dict()
         distance = dict()
+        factor = dict()
 
     inputs = Inputs()
-    inputs.Locations = Locations
-    inputs.Resources = Resources
+    inputs.locations = locations
+    inputs.resources = resources
     inputs.demand = demand
     inputs.availabilities = availabilities
     inputs.distance = distance
+    inputs.factor = factor
 
     model = Model()
     model.run(inputs)
